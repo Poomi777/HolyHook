1f42113d36a9daf173590daf2355652e65e39a20
AgustM22 <agust22@ru.is>
2024-03-21 09:04:56 +0000

    Finished playtest level, for real this time

--- Assets/Scripts/DeathCollider.cs
+++ Assets/Scripts/DeathCollider.cs
@@ -1,20 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathCollider : MonoBehaviour
 {
-
-    public Vector3 StartPosition;
-    public GameObject Player;
-
-    void Start()
-    {
-        StartPosition = Player.transform.position;
-    }
-
     void OnTriggerEnter()
     {
-        Player.transform.position = StartPosition;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
